fbed363e19d5093e2e5820b5f6f35647b23ef475
willyautoman <47789371+willyautoman@users.noreply.github.com>
2020-08-23 22:38:50 +0800

    现在测星座运势全部可以使用了！

--- Utils/utils.py
+++ Utils/utils.py
@@ -1,10 +1,9 @@
 import json
+import random
 import re
 import requests
-
-token = ""
-cjx_key = ''
-xzys_key= ''
+import os
+token = "willyautoman"
 
 def get_nmsl():
     url = 'https://nmsl.shadiao.app/api.php?from=' + token
@@ -37,13 +36,13 @@ def get_djt():
 
 
 def get_cjx(msg):
-    QQ = msg.FromQQ
-    url = 'http://japi.juhe.cn/qqevaluate/qq?key='+ cjx_key + '&qq='+str(QQ)
+    QQ = msg.FromUserId
+    url = 'http://japi.juhe.cn/qqevaluate/qq?key=c57c2f6ccb2bc93244c763bd399987df&qq='+str(QQ)
     res = requests.get(url=url)
     result = json.loads(res.text)
     conclusion = result['result']['data']['conclusion']
     analysis = result['result']['data']['analysis']
-    text = '被测QQ号：'+str(QQ)+'\r\n\r\n' + '测试结论：' + conclusion + '\r\n\r\n' + '结果分析：' + analysis + ""
+    text = '被测QQ号：'+str(QQ)+'\r\n' + '测试结论：' + conclusion + '\r\n' + '结果分析：' + analysis + ""
     return text
 
 
@@ -62,31 +61,33 @@ def get_xzys(xing_zuo, dtype):
         return "使用说明：请输入**座今日/明日/周/月/年运势（例如：狮子座今日运势、狮子座周运势等）"
     url = 'http://web.juhe.cn:8080/constellation/getAll'
     params = {
-        'key': xzys_key,
+        'key': '894dba8749130fb4163ec298cecca934',
         'consName': xing_zuo,
         'type': type
     }
     res = requests.get(url=url,params=params)
     result = json.loads(res.text)
+    text = list()
     if dtype == '今日' or dtype == '明日':
-        text = '星座名称: ' + result['name'] + '\r\n\r\n' + '日期: ' + result['datetime'] + '\r\n\r\n' + \
-               '综合指数: ' + result['all'] + '\r\n\r\n' + '健康指数: ' + result['health'] + '\r\n\r\n' + \
-               '爱情指数: ' + result['love'] + '\r\n\r\n' + '财运指数: ' + result['money'] + '\r\n\r\n' + \
-               '工作指数: ' + result['work'] + '\r\n\r\n' +'幸运色: ' + result['color'] + '\r\n\r\n' + \
-               '幸运数字: ' + str(result['number']) + '\r\n\r\n'+ '概述: ' + result['summary']
+        text.append('星座名称: ' + result['name'] + '\r\n' + '日期: ' + result['datetime'] + '\r\n' + \
+               '综合指数: ' + result['all'] + '\r\n' + '健康指数: ' + result['health'] + '\r\n' + \
+               '爱情指数: ' + result['love'] + '\r\n' + '财运指数: ' + result['money'] + '\r\n' + \
+               '工作指数: ' + result['work'] + '\r\n' +'幸运色: ' + result['color'] + '\r\n' + \
+               '幸运数字: ' + str(result['number']) + '\r\n'+ '概述: ' + result['summary'])
     elif dtype == '周':
-        text = '星座名称: ' + result['name'] + '\r\n\r\n' + '日期: ' + result['date'] + '\r\n\r\n' + \
-               '学业/求职: ' + result['job'] + '\r\n\r\n' + '爱情: ' + result['love'] + '\r\n\r\n' + \
-               '财运: ' + result['money'] + '\r\n\r\n' + '工作: ' + result['work'] + '\r\n\r\n'
+        text.append('星座名称: ' + result['name'] + '\r\n' + '日期: ' + result['date'] + '\r\n' + \
+               '学业/求职: ' + result['job'] + '\r\n' + '爱情: ' + result['love'] + '\r\n' + \
+               '财运: ' + result['money'] + '\r\n' + '工作: ' + result['work'])
     elif dtype == '月':
-        text = '星座名称: ' + result['name'] + '\r\n\r\n'+ '日期: ' + result['date'] + '\r\n\r\n' + result['all'] + '\r\n\r\n' + \
-               result['health'] + '\r\n\r\n' + result['love'] + '\r\n\r\n' + result['money'] + '\r\n\r\n' + \
-               result['work']
+        text.append('星座名称: ' + result['name'] + '\r\n'+ '日期: ' + result['date'] + '\r\n' + result['all'] + '\r\n' + \
+               result['health'])
+        text.append(result['love'] + '\r\n' + result['money'] + '\r\n' + \
+               result['work'])
     elif dtype == '年':
-        text = '星座名称: ' + result['name'] + '\r\n\r\n'+ '日期: ' + result['date'] + '\r\n\r\n' + \
-               '年度密码:①概述: ' + result['mima']['info'] + '\r\n' + '②说明: ' + result['mima']['text'][0]+ '\r\n\r\n' + \
-               result['career'][0] + '\r\n\r\n' + result['love'][0] + '\r\n\r\n' + result['health'][0] + '\r\n\r\n' + \
-               result['finance'][0]
+        text.append('星座名称: ' + result['name'] + '\r\n'+ '日期: ' + result['date'] + '\r\n' + \
+               '年度密码:①概述: ' + result['mima']['info'] + '\r\n' + '②说明: ' + result['mima']['text'][0])
+        text.append(result['career'][0] + '\r\n' + result['love'][0] + '\r\n' + result['health'][0] + '\r\n' + \
+               result['finance'][0])
     print(res.text)
     return text
 
@@ -98,9 +99,11 @@ def get_comment():
 
 
 if __name__ == '__main__':
+    # sent_wyy()
     pattern = re.compile(r'#(.*?座)(.*?)运势')
     m = pattern.match("#白羊座今日运势")
     if m is not None:
         get_xzys(m.group(1), m.group(2))
     else:
         print("参数错误")
+    # get_comment()
